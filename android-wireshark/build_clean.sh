7f6456be9b7e7916cd0cf1bb34677ead80048604
George Nychis <gnychis@gmail.com>
2012-03-07 00:10:19 -0800

    removing a bunch of stuff, updating build script to perform important autogen.sh command

--- android-wireshark/build_clean.sh
+++ android-wireshark/build_clean.sh
@@ -3,14 +3,19 @@
 # Setup the environment
 export CROSS_COMPILE=
 export ARCH=
-export CC=$(pwd)/agcc
+export CC=
+#export CC=$(pwd)/agcc
+
+find . -name Makefile -exec rm -f {} \;
+find . -name .deps -exec rm -fr {} \;
+./autogen.sh
 
 # Configure wireshark
 #make clean
 cd tools/lemon/
 gcc -D_U_=""   -o lemon lemon.c
 cd ../../
-./configure -host=arm-eabi --disable-wireshark --with-pcap=yes --disable-glibtest --with-gcrypt=no
+./configure --disable-wireshark --with-pcap=yes --disable-glibtest --with-gcrypt=no
 find . -name Makefile -exec sed -i 's/-pthread//g' {} \;
 find . -name Makefile -exec sed -i 's/-lrt/-lgcc -lpcap/g' {} \;
 find . -name Makefile -exec sed -i 's/-g -O2/-g/g' {} \;
