fd76f91a291e0aeeba6852dc7770a36bf11046a0
George Nychis <gnychis@gmail.com>
2012-03-08 08:46:55 -0800

    iNow support libgcrypt and libgpg-error

--- ported-deps/jni/Android.mk
+++ ported-deps/jni/Android.mk
@@ -2,5 +2,8 @@ LOCAL_PATH := $(call my-dir)
 subdirs := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
 	libgpg-error \
 	libgcrypt \
+	libnl \
+	libpcap \
+	libglib \
 ))
 include $(subdirs)
