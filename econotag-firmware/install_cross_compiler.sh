6d4c49b2c96377962feb580ad62d9d89d721e8ec
George Nychis <gnychis@gmail.com>
2012-03-12 20:30:52 -0700

    adding a reminder to fix the path

--- econotag-firmware/install_cross_compiler.sh
+++ econotag-firmware/install_cross_compiler.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 wget http://www.codesourcery.com/sgpp/lite/arm/portal/package3688/public/arm-none-eabi/arm-2008q3-66-arm-none-eabi.bin
+
+echo ""
+echo "--------------------------"
+echo " Choose Minimal Install"
+echo "--------------------------"
+echo ""
+
 sh arm-2008q3-66-arm-none-eabi.bin
+
+echo ""
+echo "--------------------------"
+echo " Do not forget to update PATH with /your/path/to/CodeSourcery/Sourcery_G++_Lite/bin"
+echo "--------------------------"
+echo ""
