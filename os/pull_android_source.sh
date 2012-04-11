7d6a2fe02281fecbfb8fbee37d8a888068a1a81a
George Nychis <gnychis@gmail.com>
2012-04-11 17:00:12 -0400

    only pull down the icecream sandwich code

--- os/pull_android_source.sh
+++ os/pull_android_source.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bin/repo init -u https://android.googlesource.com/platform/manifest
+bin/repo init -u https://android.googlesource.com/platform/manifest -b android-4.0.4_r1.1
 bin/repo sync
