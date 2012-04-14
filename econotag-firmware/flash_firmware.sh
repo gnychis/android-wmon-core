f061a5fd7d9d1d3f8c4c2692b9d46e0b5f3949f5
George Nychis <gnychis@gmail.com>
2012-04-14 13:05:39 -0400

    fixing some things in the core like erasing before flashing on firmware

--- econotag-firmware/flash_firmware.sh
+++ econotag-firmware/flash_firmware.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo ./libmc1322x/tools/ftditools/bbmc -l redbee-econotag erase
 sudo ./libmc1322x/tools/mc1322x-load.pl -f libmc1322x/tests/flasher_redbee-econotag.bin -s $1 -t /dev/ttyUSB1
