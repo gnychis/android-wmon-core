53ca4fa45d0a29355f07be53d39b117a9523d96d
George Nychis <gnychis@gmail.com>
2012-04-16 02:39:02 -0400

    removing platform tools to put new one in

--- android-sdk-linux/pull_platform_tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-platform=$(./tools/android list sdk | grep "SDK Platform Android 4.0.3, API 15" | awk -F'-' '{print $1}' | sed 's/^ *//g')
-gapis=$(./tools/android list sdk | grep "Google APIs, Android API 15" | awk -F'-' '{print $1}' | sed 's/^ *//g')
-
-tools/android update sdk --no-ui --filter 1,2,$platform,$gapis
