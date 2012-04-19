ae2273e8cf9e2c5a6066642160eec62bf786d04b
George Nychis <gnychis@gmail.com>
2012-04-19 12:27:13 -0400

    no need to assume 4 cores...

--- compat-wireless-2012-04-15/make.sh
+++ compat-wireless-2012-04-15/make.sh
@@ -2,4 +2,4 @@
 make clean
 ./scripts/driver-select ath9k
 KERNEL_DIR=$(pwd)/../usbhost-kernel
-make -j4 ARCH=arm CROSS_COMPILE=arm-eabi- KLIB=$KERNEL_DIR KLIB_BUILD=$KERNEL_DIR "$@"
+make ARCH=arm CROSS_COMPILE=arm-eabi- KLIB=$KERNEL_DIR KLIB_BUILD=$KERNEL_DIR "$@"
