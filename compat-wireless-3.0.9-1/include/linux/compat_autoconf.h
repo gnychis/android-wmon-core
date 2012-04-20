02bba4bb50ee37ec0be5ca4e3ffb4263b23ce7cf
George Nychis <gnychis@gmail.com>
2012-04-20 13:43:16 -0400

    updating the kernel

--- compat-wireless-3.0.9-1/include/linux/compat_autoconf.h
+++ compat-wireless-3.0.9-1/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Thu Apr 19 15:06:26 EDT 2012 
+ * Fri Apr 20 13:26:57 EDT 2012 
  * compat-wireless-2.6: compat-wireless-v3.0.9-1
  * linux-2.6: v3.0.9
  */
