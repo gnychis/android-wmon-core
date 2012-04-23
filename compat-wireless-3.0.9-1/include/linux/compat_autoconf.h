0af4a8c12710ef4f35207202a9d80847bdf3e12c
George Nychis <gnychis@gmail.com>
2012-04-22 23:21:35 -0400

    adding in changes to compat-wireless to add our USB device ID, otherwise it does not get picked up by the ath9k kernel modules

--- compat-wireless-3.0.9-1/include/linux/compat_autoconf.h
+++ compat-wireless-3.0.9-1/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Fri Apr 20 13:26:57 EDT 2012 
+ * Sun Apr 22 23:14:40 EDT 2012 
  * compat-wireless-2.6: compat-wireless-v3.0.9-1
  * linux-2.6: v3.0.9
  */
