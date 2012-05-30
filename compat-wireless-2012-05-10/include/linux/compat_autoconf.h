facf4ba6a8fab24a09890beb3ea5298776af2714
George Nychis <gnychis@gmail.com>
2012-05-30 17:59:56 -0400

    updating the sgs2 kernel for the new rt2800 firmware

--- compat-wireless-2012-05-10/include/linux/compat_autoconf.h
+++ compat-wireless-2012-05-10/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Wed May 30 16:54:12 EDT 2012 
+ * Wed May 30 16:58:09 EDT 2012 
  * compat-wireless-2.6: compat-wireless-2012-05-04
  * linux-2.6: next-20120510
  */
