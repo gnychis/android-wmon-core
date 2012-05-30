059b10a09823e22039054d3abbfa4ff6fd72f448
George Nychis <gnychis@gmail.com>
2012-05-30 01:24:51 -0400

    updating the skyrocket kernel

--- compat-wireless-2012-04-26/include/linux/compat_autoconf.h
+++ compat-wireless-2012-04-26/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Sun May 27 11:48:27 EDT 2012 
+ * Tue May 29 20:54:14 EDT 2012 
  * compat-wireless-2.6: compat-wireless-2012-04-24-1
  * linux-2.6: next-20120424
  */
