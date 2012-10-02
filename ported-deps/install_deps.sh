a0e18fafb19f1aadd12a6a592994afc4abe2bb1f
George Nychis <gnychis@gmail.com>
2012-10-01 23:57:41 -0400

    need to switch ndk

--- ported-deps/install_deps.sh
+++ ported-deps/install_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp jni/libpcap/scanner_good.c jni/libpcap/scanner.c
 cp jni/libpcap/grammar_good.c jni/libpcap/grammar.c
-../android-ndk-r7b-linux/ndk-build
-cp libs/armeabi/*.so ../android-ndk-r7b-linux/platforms/android-9/arch-arm/usr/lib/
+../android-ndk-r8b-linux/ndk-build
+cp libs/armeabi/*.so ../android-ndk-r8b-linux/platforms/android-14/arch-arm/usr/lib/
