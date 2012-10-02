b95cfaa9adee3d5cc98d2bd70ad9a5e16261439e
George Nychis <gnychis@gmail.com>
2012-10-02 15:24:56 -0400

    adding back in the android sdk

---
+++ android-sdk-linux/pull_platform_tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+platform=$(./tools/android list sdk | grep "SDK Platform Android" | head -n 1 | awk -F'-' '{print $1}' | sed 's/^ *//g')
+gapis=$(./tools/android list sdk | grep "Google APIs" | head -n 1 | awk -F'-' '{print $1}' | sed 's/^ *//g')
+
+echo $platform
+echo $gapis
+tools/android update sdk --no-ui --filter 1,2,$platform,$gapis
