eeaf55e9f206c08f09d330b83ceef996e84a310f
George Nychis <gnychis@gmail.com>
2012-10-03 09:56:27 -0400

    updating to newest version of os

--- os/pull_android_source.sh
+++ os/pull_android_source.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bin/repo init -u https://android.googlesource.com/platform/manifest -b android-4.1.1_r6
+bin/repo init -u https://android.googlesource.com/platform/manifest 
 bin/repo sync
