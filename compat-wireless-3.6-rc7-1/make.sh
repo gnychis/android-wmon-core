df8672f97cc6cf4be67e497147c1df2192d3ed9c
George Nychis <gnychis@gmail.com>
2012-10-11 12:05:52 -0700

    adding in compat-wireless for galaxy nexus

---
+++ compat-wireless-3.6-rc7-1/make.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+make clean
+./scripts/driver-select restore
+./scripts/driver-select rt2x00
+#./scripts/driver-select brcm80211
+#KERNEL_DIR=$(pwd)/../usbhost-kernel
+#KERNEL_DIR=$(pwd)/../samsung-kernel-galaxysii
+KERNEL_DIR=$(pwd)/../galaxynexus-cm10-kernel
+make ARCH=arm CROSS_COMPILE=arm-linux-androideabi- KLIB=$KERNEL_DIR KLIB_BUILD=$KERNEL_DIR "$@" EXTRA_CFLAGS=-fno-pic
