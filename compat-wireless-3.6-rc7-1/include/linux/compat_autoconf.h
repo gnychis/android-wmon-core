288d5f7672703f3d58463fe3bc40576556e3de6c
George Nychis <gnychis@gmail.com>
2012-10-12 08:59:28 -0700

    removing o files

--- compat-wireless-3.6-rc7-1/include/linux/compat_autoconf.h
+++ compat-wireless-3.6-rc7-1/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Fri Oct 12 08:02:07 PDT 2012 
+ * Fri Oct 12 08:50:24 PDT 2012 
  * compat-wireless-2.6: compat-wireless-v3.6-rc7-1
  * linux-2.6: v3.6-rc7
  */
