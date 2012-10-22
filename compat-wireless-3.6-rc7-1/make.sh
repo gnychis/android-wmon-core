f22e2893ea480ea4891db154d46b717629def7e8
George Nychis <gnychis@gmail.com>
2012-10-21 20:22:21 -0700

    updating the makefile

--- compat-wireless-3.6-rc7-1/make.sh
+++ compat-wireless-3.6-rc7-1/make.sh
@@ -6,5 +6,6 @@ make clean
 #./scripts/driver-select brcm80211
 #KERNEL_DIR=$(pwd)/../usbhost-kernel
 #KERNEL_DIR=$(pwd)/../samsung-kernel-galaxysii
+cp drivers/net/wireless/Makefile.full drivers/net/wireless/Makefile
 KERNEL_DIR=$(pwd)/../galaxynexus-cm10-kernel
 make ARCH=arm CROSS_COMPILE=arm-linux-androideabi- KLIB=$KERNEL_DIR KLIB_BUILD=$KERNEL_DIR "$@" EXTRA_CFLAGS=-fno-pic
