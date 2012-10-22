6ce0e48838172b1dabc05f1ecfdb8c4a1c21de28
George Nychis <gnychis@gmail.com>
2012-10-22 14:45:09 -0700

    adding in compat wireless for sgs3 and an updated kernel

---
+++ compat-wireless-sgs3/make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+make clean
+./scripts/driver-select restore
+./scripts/driver-select bcmdhd
+#./scripts/driver-select rt2x00
+#./scripts/driver-select brcm80211
+#KERNEL_DIR=$(pwd)/../usbhost-kernel
+#KERNEL_DIR=$(pwd)/../samsung-kernel-galaxysii
+cp drivers/net/wireless/Makefile.full drivers/net/wireless/Makefile
+KERNEL_DIR=$(pwd)/../sgs3-cm10-kernel
+make ARCH=arm CROSS_COMPILE=arm-linux-androideabi- KLIB=$KERNEL_DIR KLIB_BUILD=$KERNEL_DIR "$@" EXTRA_CFLAGS=-fno-pic
