bca0a9c61ea343b54626ceb847e2bd64ec59e6b3
George Nychis <gnychis@gmail.com>
2012-11-07 13:40:32 -0800

    lets just get rid of this thing forever

--- compat-wireless-3.6-rc7-1/include/linux/compat_autoconf.h
+++ compat-wireless-3.6-rc7-1/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Wed Nov  7 13:13:01 PST 2012 
+ * Wed Nov  7 13:35:59 PST 2012 
  * compat-wireless-2.6: compat-wireless-v3.6-rc7-1
  * linux-2.6: v3.6-rc7
  */
